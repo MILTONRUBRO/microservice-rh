377161dfdbb59fdce7e854d2d3cd4362981b8e56
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-01-24 21:58:03 -0200

    Iniciando projeto hr-worker

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Richard', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Gilfoyle', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Dinesh', 250.0);
