852b51c6414be56cc41c1e11ddc64534fcb2c3c4
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-01-27 23:15:48 -0200

    Adicionando rest template para devolver um objeto worker

--- hr-payrool/src/main/java/com/devmos/hrpayrool/services/PaymentService.java
+++ hr-payrool/src/main/java/com/devmos/hrpayrool/services/PaymentService.java
@@ -1,14 +1,35 @@
 package com.devmos.hrpayrool.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devmos.hrpayrool.entities.Payment;
+import com.devmos.hrpayrool.entities.Worker;
 
 @Service
 public class PaymentService {
 	
-	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Jared", 200.0, days);
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public Payment getPayment(Long workerId, Integer days) {		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, getUriVariables(workerId));
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+
+	private Map<String, String> getUriVariables(Long workerId) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+		
+		return uriVariables;
 	}
 
 }
