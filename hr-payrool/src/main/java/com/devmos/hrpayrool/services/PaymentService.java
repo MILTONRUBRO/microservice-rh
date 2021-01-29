cb21f9e4cdfac6b9c2f536f0f028beb7b50595f4
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-01-28 23:09:10 -0200

    Adicionando Feign para comunicacao dos projetos

--- hr-payrool/src/main/java/com/devmos/hrpayrool/services/PaymentService.java
+++ hr-payrool/src/main/java/com/devmos/hrpayrool/services/PaymentService.java
@@ -1,35 +1,21 @@
 package com.devmos.hrpayrool.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devmos.hrpayrool.entities.Payment;
 import com.devmos.hrpayrool.entities.Worker;
+import com.devmos.hrpayrool.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, Integer days) {		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, getUriVariables(workerId));
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-	}
-
-	private Map<String, String> getUriVariables(Long workerId) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-		
-		return uriVariables;
 	}
 
 }
