ce3adcc215007724ab4945287ecf789c09d60f9d
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-01-30 14:41:37 -0200

    Adicionando e configurando servidor eureka

---
+++ hr-eureka-server/src/test/java/com/devmos/hreureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmos.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
