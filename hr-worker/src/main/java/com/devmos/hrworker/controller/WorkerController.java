e01a513d62a68ae7fb4acd7ca4b9b3a7a18b9c2d
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-02-10 19:42:32 -0200

    Adicionando configuracao do actuator

--- hr-worker/src/main/java/com/devmos/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devmos/hrworker/controller/WorkerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.devmos.hrworker.entities.Worker;
 import com.devmos.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping("api/workers")
 public class WorkerController {
